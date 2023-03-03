6a8cf2047880d6937c21e3e7d7c3a6b92e89d5a6
ABHAYSHARMA <as7766700@gmail.com>
2023-03-03 16:06:34 +0530

    Lats

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  const addToList = () => {
+    if(input.length){
+      setList((prev)=>[...prev,input])
+      setInput("");
+    }
+    console.log("list--->", list);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input val={input} onChange={setInput}
+      />
+      <button onSubmit={addToList}>submit</button>
+      {list.map((item,ind)=>{
+        return <div key={ind}>{item}</div>
+      })}
     </div>
   );
 }
