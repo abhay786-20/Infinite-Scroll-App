ac3c3cccb496ca8d27026bcecf116ff8cdf791ba
ABHAYSHARMA <as7766700@gmail.com>
2023-05-13 15:34:42 +0530

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
-import React, {useState} from "react";
-
+import React from 'react';
+import './App.css'
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Navv from './components/Navv'
+import Login from './components/Loginn/Login'
+import PrivateComponent from './PrivateComponent';
+import Home from './components/Home';
 function App() {
-  const [list, setList] = useState([]);
-  const [input, setInput] = useState("");
-  const addToList = () => {
-    if(input.length){
-      setList((prev)=>[...prev,input])
-      setInput("");
-    }
-    console.log("list--->", list);
-  }
   return (
-    <div className="App">
-      <input val={input} onChange={setInput}
-      />
-      <button onSubmit={addToList}>submit</button>
-      {list.map((item,ind)=>{
-        return <div key={ind}>{item}</div>
-      })}
+    <div className='App'>
+     <BrowserRouter>
+      <Navv/>
+      <Routes>
+        <Route element = {<PrivateComponent/>}>
+        <Route path="/" element = {<Home/>}/>
+        </Route>
+        <Route path ="/login" element = {<Login/>}/>
+      </Routes>
+     </BrowserRouter>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
